6d7e81a330a1006221322a44b896d0070206b797
metoerboyF <gafardeen223321@bscse.uiu.ac.bd>
2026-05-02 04:02:46 +0600

    fix: mvnw.cmd strip trailing backslash from MAVEN_PROJECTBASEDIR

--- pangochain-backend/mvnw.cmd
+++ pangochain-backend/mvnw.cmd
@@ -1,10 +1,13 @@
 @echo off
 setlocal
 
-SET MAVEN_PROJECTBASEDIR=%~dp0
-SET WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar
-SET WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
-SET DOWNLOAD_URL=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar
+:: Get script directory, strip trailing backslash
+SET "MAVEN_PROJECTBASEDIR=%~dp0"
+IF "%MAVEN_PROJECTBASEDIR:~-1%"=="\" SET "MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%"
+
+SET "WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
+SET "WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain"
+SET "DOWNLOAD_URL=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar"
 
 IF NOT EXIST "%WRAPPER_JAR%" (
     echo Downloading Maven wrapper jar...
@@ -16,9 +19,9 @@ IF NOT EXIST "%WRAPPER_JAR%" (
 )
 
 IF NOT "%JAVA_HOME%" == "" (
-    SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
+    SET "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
 ) ELSE (
-    SET JAVA_EXE=java
+    SET "JAVA_EXE=java"
 )
 
-"%JAVA_EXE%" -classpath "%WRAPPER_JAR%" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" %WRAPPER_LAUNCHER% %*
+"%JAVA_EXE%" -classpath "%WRAPPER_JAR%" -Dmaven.multiModuleProjectDirectory="%MAVEN_PROJECTBASEDIR%" %WRAPPER_LAUNCHER% %*
